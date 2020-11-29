c9e5a58bcdf245ad59769c1e6b4f59636957240b
him-2c <5635512031@psu.ac.th>
2020-11-29 23:36:34 +0700

    - add, eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import React from 'react';
+import {AppProps} from 'next/app';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = (ctx: AppProps) => {
+  const {Component, pageProps} = ctx;
+  return <Component {...pageProps} />;
+};
 
-export default MyApp
+export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
@@ -13,23 +14,19 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
-
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
-
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
@@ -37,7 +34,6 @@ export default function Home() {
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
-
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
@@ -61,5 +57,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
